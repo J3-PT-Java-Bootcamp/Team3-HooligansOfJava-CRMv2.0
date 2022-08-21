1b7f34c872ca7e1e925dd5fa1f5282813c19d5db
Ana Montero <anamongar@gmail.com>
2022-08-21 21:22:14 +0200

    Include repository annotation

--- src/main/java/com/ironhack/repository/LeadRepository.java
+++ src/main/java/com/ironhack/repository/LeadRepository.java
@@ -2,6 +2,8 @@ package com.ironhack.repository;
 
 import com.ironhack.model.Lead;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LeadRepository extends JpaRepository<Lead, Long> {
 }
--- src/main/java/com/ironhack/repository/OpportunityRepository.java
+++ src/main/java/com/ironhack/repository/OpportunityRepository.java
@@ -2,6 +2,8 @@ package com.ironhack.repository;
 
 import com.ironhack.model.Opportunity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OpportunityRepository extends JpaRepository<Opportunity, Long> {
 }
