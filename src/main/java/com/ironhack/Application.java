bc25636c1b3cbfe6c5c2fe62111026b1b847685a
Joaquim Pinda <joaquimpineda@gmail.com>
2022-08-24 22:24:28 +0200

    reporting initial menu

--- src/main/java/com/ironhack/Application.java
+++ src/main/java/com/ironhack/Application.java
@@ -1,13 +1,25 @@
 package com.ironhack;
 
+import com.ironhack.console.ConsoleBuilder;
+import com.ironhack.reporting.Reporting;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class Application {
+public class Application  implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    public void run(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ConsoleBuilder consoleBuilder = new ConsoleBuilder(scanner);
+        Reporting reports = new Reporting(consoleBuilder);
+        reports.menu();
+    }
 }
