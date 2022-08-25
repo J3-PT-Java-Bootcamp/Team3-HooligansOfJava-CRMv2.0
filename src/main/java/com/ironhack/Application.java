162e02359eeb1f4142318793ca24c8dcaff6bb94
Ana Montero <anamongar@gmail.com>
2022-08-25 18:24:21 +0200

    Merge develop into menu branch

--- src/main/java/com/ironhack/Application.java
+++ src/main/java/com/ironhack/Application.java
@@ -1,7 +1,10 @@
 package com.ironhack;
 
 import com.ironhack.console.ConsoleBuilder;
+import com.ironhack.menu.Menu;
 import com.ironhack.reporting.Reporting;
+import com.ironhack.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,15 +12,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Scanner;
 
 @SpringBootApplication
-public class Application  implements CommandLineRunner {
+public class Application implements CommandLineRunner {
+    // Services
+    @Autowired
+    SalesRepService salesRepService;
+    @Autowired
+    LeadService leadService;
+    @Autowired
+    OpportunityService opportunityService;
+    @Autowired
+    ContactService contactService;
+    @Autowired
+    AccountService accountService;
+    @Autowired
+    ProductService productService;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Override
-    public void run(String[] args) {
+    public void run(String[] args) throws InterruptedException {
+
         Scanner scanner = new Scanner(System.in);
+        Menu menu = new Menu(salesRepService, leadService, opportunityService, contactService, accountService, productService);
+        menu.start();
+
         ConsoleBuilder consoleBuilder = new ConsoleBuilder(scanner);
         Reporting reports = new Reporting(consoleBuilder);
         reports.menu();
