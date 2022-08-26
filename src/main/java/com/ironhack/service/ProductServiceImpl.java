cf24e3f8691e3a7435b716eca1c5214aaeebcfd1
Joaquim Pinda <joaquimpineda@gmail.com>
2022-08-27 01:32:35 +0200

    add median function

--- src/main/java/com/ironhack/service/ProductServiceImpl.java
+++ src/main/java/com/ironhack/service/ProductServiceImpl.java
@@ -8,13 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 @Service
 public class ProductServiceImpl implements ProductService {
     @Autowired
     ProductRepository productRepository;
+
     @Override
     public Product newProduct(TypeOfProduct type, int quantity) {
         Product product = new Product();
@@ -27,12 +27,12 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void reportOpportunity() {
         var salesRepResponse = productRepository.groupOpportunityByName();
-        String[][] data = new String[salesRepResponse.size()+1][2];
+        String[][] data = new String[salesRepResponse.size() + 1][2];
         data[0][0] = "Product";
         data[0][1] = "Number ";
         for (int i = 0; i < salesRepResponse.size(); i++) {
-            data[i+1][0] =salesRepResponse.get(i)[0].toString();
-            data[i+1][1] =salesRepResponse.get(i)[1].toString();
+            data[i + 1][0] = salesRepResponse.get(i)[0].toString();
+            data[i + 1][1] = salesRepResponse.get(i)[1].toString();
         }
         tableWithLinesAndMaxWidth.tableWithLinesAndMaxWidth(data);
     }
@@ -40,12 +40,12 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void reportClosedWon() {
         var salesRepResponse = productRepository.groupOpportunityByNameWon();
-        String[][] data = new String[salesRepResponse.size()+1][2];
+        String[][] data = new String[salesRepResponse.size() + 1][2];
         data[0][0] = "Product";
         data[0][1] = "Number of opportunities won";
         for (int i = 0; i < salesRepResponse.size(); i++) {
-            data[i+1][0] =salesRepResponse.get(i)[0].toString();
-            data[i+1][1] =salesRepResponse.get(i)[1].toString();
+            data[i + 1][0] = salesRepResponse.get(i)[0].toString();
+            data[i + 1][1] = salesRepResponse.get(i)[1].toString();
         }
         tableWithLinesAndMaxWidth.tableWithLinesAndMaxWidth(data);
 
@@ -54,12 +54,12 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void reportClosedLost() {
         var salesRepResponse = productRepository.groupOpportunityByNameLost();
-        String[][] data = new String[salesRepResponse.size()+1][2];
+        String[][] data = new String[salesRepResponse.size() + 1][2];
         data[0][0] = "Products";
         data[0][1] = "Number of opportunities lost";
         for (int i = 0; i < salesRepResponse.size(); i++) {
-            data[i+1][0] =salesRepResponse.get(i)[0].toString();
-            data[i+1][1] =salesRepResponse.get(i)[1].toString();
+            data[i + 1][0] = salesRepResponse.get(i)[0].toString();
+            data[i + 1][1] = salesRepResponse.get(i)[1].toString();
         }
         tableWithLinesAndMaxWidth.tableWithLinesAndMaxWidth(data);
 
@@ -68,12 +68,12 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void reportOpen() {
         var salesRepResponse = productRepository.groupOpportunityByNameOpen();
-        String[][] data = new String[salesRepResponse.size()+1][2];
+        String[][] data = new String[salesRepResponse.size() + 1][2];
         data[0][0] = "Products";
         data[0][1] = "Number of opportunities open";
         for (int i = 0; i < salesRepResponse.size(); i++) {
-            data[i+1][0] =salesRepResponse.get(i)[0].toString();
-            data[i+1][1] =salesRepResponse.get(i)[1].toString();
+            data[i + 1][0] = salesRepResponse.get(i)[0].toString();
+            data[i + 1][1] = salesRepResponse.get(i)[1].toString();
         }
         tableWithLinesAndMaxWidth.tableWithLinesAndMaxWidth(data);
     }
@@ -81,81 +81,72 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void meanProductQuantity() {
         var response = productRepository.findMeanProductQuantity();
-        String[][] data = new String[response.size()+1][2];
+        String[][] data = new String[response.size() + 1][2];
         data[0][0] = "Products";
         data[0][1] = "Mean quantity";
         for (int i = 0; i < response.size(); i++) {
-            data[i+1][0] =response.get(i)[0].toString();
-            data[i+1][1] =response.get(i)[1].toString();
+            data[i + 1][0] = response.get(i)[0].toString();
+            data[i + 1][1] = response.get(i)[1].toString();
         }
         tableWithLinesAndMaxWidth.tableWithLinesAndMaxWidth(data);
     }
 
     @Override
     public void medianProductQuantity() {
-//        var allProducts = productRepository.findMedianProductQuantity();
-//        var allQuantities = allProducts.get(1);
-//        ArrayList<Object[]> response = new ArrayList<>();
-//        int median;
-//
-//        String productType = "";
-//        int count = 0;
-//        ArrayList<Integer> quantity = new ArrayList<>();
-//        for (int i = 0; i < allProducts.size(); i++) {
-//            var product = allProducts.get(i);
-//
-//            if(productType.equals(product[0])){
-//                count++;
-//                quantity.add((Integer) product[1]);
-//            } else {
-//                System.out.println(i);
-//                if(i>1) {
-//                    int mitad = count / 2;
-//                    if (quantity.size() % 2 == 0) {
-//                        median = (quantity.get(mitad - 1) + quantity.get(mitad) / 2);
-//                    } else {
-//                        median = quantity.get(mitad);
-//                    }
-//
-//                    response.add(new Object[]{productType, median});
-//                }
-//                count = 1;
-//                productType =(String) product[0];
-//                quantity.clear();
-//                quantity.add((Integer) product[1]);
-//            }
-//            int mitad = count / 2;
-//            if (quantity.size() % 2 == 0) {
-//                median = (quantity.get(mitad - 1) + quantity.get(mitad) / 2);
-//            } else {
-//                median = quantity.get(mitad);
-//            }
-//            response.add(new Object[]{productType,median});
-//        }
-//        int mitad = count / 2;
-//        if (quantity.size() % 2 == 0) {
-//            median = (quantity.get(mitad - 1) + quantity.get(mitad) / 2);
-//        } else {
-//            median = quantity.get(mitad);
-//        }
-//        response.add(new Object[]{productType,median});
-//        String[][] array = new String[response.size()][2];
-//        for (int i = 0; i < response.size(); i++){
-//            array[i][0] = (String) response.get(i)[0];
-//            array[i][1] = (String) response.get(i)[1];
-//        }
-//        tableWithLinesAndMaxWidth.tableWithLinesAndMaxWidth(array);
+        var allProducts = productRepository.findMedianProductQuantity();
+        ArrayList<Object[]> response = new ArrayList<>();
+        int median;
+        String productType = "";
+        int count = 0;
+        ArrayList<Integer> quantity = new ArrayList<>();
+        for (int i = 0; i < allProducts.size(); i++) {
+            var product = allProducts.get(i);
+            if (productType.equals((String) product[0])) {
+                count++;
+                quantity.add((Integer) product[1]);
+            } else {
+                if (i > 1) {
+                    int mitad = count / 2;
+                    if (quantity.size() % 2 == 0) {
+                        median = (quantity.get(mitad - 1) + quantity.get(mitad) / 2);
+                    } else {
+                        median = quantity.get(mitad);
+                    }
+                    response.add(new Object[]{productType, median});
+                }
+                count = 1;
+                productType = (String) product[0];
+                quantity.clear();
+                quantity.add((Integer) product[1]);
+            }
+        }
+        int mitad = count / 2;
+        if (quantity.size() % 2 == 0) {
+            median = (quantity.get(mitad - 1) + quantity.get(mitad) / 2);
+        } else {
+            median = quantity.get(mitad);
+        }
+        response.add(new Object[]{productType, median});
+        String[][] array = new String[response.size()+1][2];
+        array[0][0] = "Products";
+        array[0][1] = "Median quantity";
+        for (int i = 0; i <= response.size()-1; i++) {
+            array[i+1][0] = (String) response.get(i)[0];
+            array[i+1][1] = (String) response.get(i)[1].toString();
+        }
+
+        tableWithLinesAndMaxWidth.tableWithLinesAndMaxWidth(array);
     }
 
     @Override
     public void maxProductQuantity() {
         var response = productRepository.findMaxProductQuantity();
-        String[][] data = new String[response.size()+1][2];
+        String[][] data = new String[response.size() + 1][2];
         data[0][0] = "Products";
         data[0][1] = "Max quantity";
         for (int i = 0; i < response.size(); i++) {
-            data[i+1][0] =response.get(i)[0].toString();
-            data[i+1][1] =response.get(i)[1].toString();
+            data[i + 1][0] = response.get(i)[0].toString();
+            data[i + 1][1] = response.get(i)[1].toString();
         }
         tableWithLinesAndMaxWidth.tableWithLinesAndMaxWidth(data);
     }
@@ -163,12 +154,12 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void minProductQuantity() {
         var response = productRepository.findMinProductQuantity();
-        String[][] data = new String[response.size()+1][2];
+        String[][] data = new String[response.size() + 1][2];
         data[0][0] = "Products";
         data[0][1] = "Max quantity";
         for (int i = 0; i < response.size(); i++) {
-            data[i+1][0] =response.get(i)[0].toString();
-            data[i+1][1] =response.get(i)[1].toString();
+            data[i + 1][0] = response.get(i)[0].toString();
+            data[i + 1][1] = response.get(i)[1].toString();
         }
         tableWithLinesAndMaxWidth.tableWithLinesAndMaxWidth(data);
     }
